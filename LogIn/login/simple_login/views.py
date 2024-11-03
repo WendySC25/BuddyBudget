8c804646596946cb7b33029773c8e8d3e645226f
WendySC <wendy.c2502@gmail.com>
2024-11-03 11:47:47 -0600

    feat: Implement user authentication views
    - Added  view to render the home page, protected by login required.
    - Created  for user registration using .
    - Configured URL patterns to include home, signup, and default Django auth URLs.

--- LogIn/login/simple_login/views.py
+++ LogIn/login/simple_login/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def home(request):
+    return render(request,"home.html", {})
+
+def authView(requets):
+    if requets.method == "POST":
+        form = UserCreationForm(requets.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("simple_login:login")
+    else:
+        form = UserCreationForm()
+
+    return render(requets, "registration/sigup.html", {"form" : form})

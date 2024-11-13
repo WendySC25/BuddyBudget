4e4e83a64a120549d14204d86af932dbc5ed85ec
WendySC <wendy.c2502@gmail.com>
2024-11-12 22:45:01 -0600

    feat(models): defined core financial models for income and expense tracking
    - Added Income model to represent user income records with fields for category, account, amount, description, and date
    - Added Expense model to represent user expense records, similar to income with associations to category and account
    - Defined Account model to manage user financial accounts, linked to an  and includes details like account name, bank name, and expiry date
    - Defined Category model to categorize income and expenses, associated with specific users
    - Defined AccountType model for account classifications (e.g., Credit, Debit, Cash) with description fields

--- backend/user_api/models.py
+++ backend/user_api/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 class AppUserManager(BaseUserManager):
-
 	def create_user(self, email, username, password=None):
 		if not email:
 			raise ValueError('An email is required.')
@@ -15,6 +14,9 @@ class AppUserManager(BaseUserManager):
 		user = self.model(email=email, username=username)
 		user.set_password(password)
 		user.save()
+
+		Profile.objects.get_or_create(user=user)
+
 		return user
 	
 
@@ -30,31 +32,81 @@ class AppUserManager(BaseUserManager):
 		return user
 
 class AppUser(AbstractBaseUser, PermissionsMixin):
-	user_id = models.AutoField(primary_key=True)
-	email = models.EmailField(max_length=50, unique=True)
-	username = models.CharField(max_length=50, unique=True)
+	user_id 	 = models.AutoField(primary_key=True)
+	email 		 = models.EmailField(max_length=50, unique=True)
+	username 	 = models.CharField(max_length=50, unique=True)
 
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False) 
+	is_active 	 = models.BooleanField(default=True)
+	is_staff     = models.BooleanField(default=False) 
 	is_superuser = models.BooleanField(default=False)
 
-	USERNAME_FIELD = 'email'	
+	USERNAME_FIELD 	= 'email'	
 	REQUIRED_FIELDS = ['username']
 	objects = AppUserManager()
 
 	def __str__(self):
 		return self.username
 
-
 class Profile(models.Model):
-	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
-
-	name = models.CharField(max_length=50, blank=True, null=True)
-	last_name = models.CharField(max_length=50, blank=True, null=True)
-	RFC = models.CharField(max_length=13, blank=True, null=True)
-	bio = models.TextField(blank=True, null=True)
+	user 			= models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile")
+	name 			= models.CharField(max_length=50, blank=True, null=True)
+	last_name 		= models.CharField(max_length=50, blank=True, null=True)
+	RFC 			= models.CharField(max_length=13, blank=True, null=True)
+	bio 			= models.TextField(blank=True, null=True)
 	# avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
-	phone_number = models.CharField(max_length=20, blank=True, null=True)
+	phone_number	= models.CharField(max_length=20, blank=True, null=True)
 
 	def __str__(self):
 		return f"Profile of {self.user.username}"
+	
+class AccountType(models.Model):
+    type_name 	= models.CharField(max_length=50)  # "Crédito", "Débito", "Efectivo"
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.type_name
+
+
+class Account(models.Model):
+    user 			= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accounts')
+    account_type 	= models.ForeignKey(AccountType, on_delete=models.SET_NULL, null=True, related_name='accounts')
+    account_name 	= models.CharField(max_length=100)  # Alias de la cuenta
+    bank_name 		= models.CharField(max_length=50, blank=True, null=True)
+    account_number 	= models.CharField(max_length=20, blank=True, null=True)
+    expiry_date 	= models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.account_name} ({self.account_type.type_name})"
+
+
+class Category(models.Model):
+	user 			= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='categories')
+	category_name 	= models.CharField(max_length=50)
+	description 	= models.TextField(blank=True, null=True)
+
+	def __str__(self):
+		return self.category_name
+
+
+class Income(models.Model):
+    user 		= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='incomes')
+    category 	= models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='incomes')
+    account 	= models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='incomes')
+    amount 		= models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date 		= models.DateField()
+
+    def __str__(self):
+        return f"{self.amount} - {self.category.name} ({self.account.account_name})"
+
+
+class Expense(models.Model):
+    user 		= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='expenses')
+    category 	= models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='expenses')
+    account 	= models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, related_name='expenses')
+    amount 		= models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
+    date 		= models.DateField()
+
+    def __str__(self):
+        return f"{self.amount} - {self.category.name} ({self.account.account_name})"

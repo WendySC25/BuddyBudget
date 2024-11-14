3dddd261e76c73bb34732b3a161a06ad40cc8d37
WendySC <wendy.c2502@gmail.com>
2024-11-13 19:53:11 -0600

    feat: add Income and Expense API views for CRUD operations

--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -3,8 +3,10 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer, ProfileUpdateSerializer
+from .serializers import IncomeSerializer, ExpenseSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
+from .models import Income, Expense
 
 
 class UserRegister(APIView):
@@ -73,3 +75,95 @@ class ProfileView(APIView):
 			serializer.save()
 			return Response({'profile': serializer.data}, status=status.HTTP_200_OK)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+
+class IncomeListCreateView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def get(self, request):
+        incomes = Income.objects.filter(user=request.user)
+        serializer = IncomeSerializer(incomes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = IncomeSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class IncomeDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def get_object(self, pk):
+        try:
+            return Income.objects.get(pk=pk, user=self.request.user)
+        except Income.DoesNotExist:
+            raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        income = self.get_object(pk)
+        serializer = IncomeSerializer(income)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        income = self.get_object(pk)
+        serializer = IncomeSerializer(income, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        income = self.get_object(pk)
+        income.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ExpenseListCreateView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def get(self, request):
+        expenses = Expense.objects.filter(user=request.user)
+        serializer = ExpenseSerializer(expenses, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ExpenseSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ExpenseDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    authentication_classes = (SessionAuthentication,)
+
+    def get_object(self, pk):
+        try:
+            return Expense.objects.get(pk=pk, user=self.request.user)
+        except Expense.DoesNotExist:
+            raise Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        expense = self.get_object(pk)
+        serializer = ExpenseSerializer(expense)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        expense = self.get_object(pk)
+        serializer = ExpenseSerializer(expense, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        expense = self.get_object(pk)
+        expense.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

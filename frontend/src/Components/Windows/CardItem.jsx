ce800fc51b2ef2da98b1319b39db6c0069b6f94f
WendySC <wendy.c2502@gmail.com>
2024-12-02 18:58:52 -0600

    wip: developing new components for the homepage

---
+++ frontend/src/Components/Windows/CardItem.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './Card.css';
+
+const CardItem = ({ image, thumb, title, status, description, onClick }) => (
+  <li>
+    <a href="#" className="card" onClick={(e) => { e.preventDefault(); onClick(); }}>
+      <img src={image} className="card__image" alt="" />
+      <div className="card__overlay">
+        <div className="card__header">
+          <svg className="card__arc" xmlns="http://www.w3.org/2000/svg">
+            <path />
+          </svg>
+          <img className="card__thumb" src={thumb} alt="" />
+          <div className="card__header-text">
+            <h3 className="card__title">{title}</h3>
+            <span className="card__status">{status}</span>
+          </div>
+        </div>
+        <p className="card__description">{description}</p>
+      </div>
+    </a>
+  </li>
+);
+
+const Card = () => {
+  const cardsData = [
+    {
+      image: 'Assets/wallpaper2.png',
+      thumb: 'https://i.imgur.com/7D7I6dI.png',
+      title: 'Panque',
+      status:  'panque',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?',
+    },
+    {
+      image: 'Assets/wallpaper2.png',
+      thumb: 'https://i.imgur.com/7D7I6dI.png',
+      title: 'Panque',
+      status:  'panque',
+      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?',
+    },
+   
+    
+  ];
+
+  const handleCardClick = (title) => {
+    console.log(`Card clicked: ${title}`);
+  };
+
+  return (
+    <ul className="cards">
+      {cardsData.map((card, index) => (
+        <CardItem
+          key={index}
+          image={card.image}
+          thumb={card.thumb}
+          title={card.title}
+          status={card.status}
+          description={card.description}
+          onClick={() => handleCardClick(card.title)}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Card;

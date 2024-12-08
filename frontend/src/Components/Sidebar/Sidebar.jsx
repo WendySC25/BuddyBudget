9f394831526358dc1d97ed9eadc0ff0635e63331
WendySC <wendy.c2502@gmail.com>
2024-12-07 22:03:46 -0600

    feat: Define route for users' accounts in the admin panel

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
                     <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/subpage1">Users</Link></a>
                     <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/transactions">Transactions</Link></a>
                     <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/categories">Categories</Link></a>
-                    <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/categories">Acounts</Link></a>
+                    <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/accounts">Acounts</Link></a>
                     <a className="sidebar-link" ><svg viewBox="0 0 24 24" fill="currentColor"/><Link to="/superhome/debts">Debt</Link></a>
                 </div>
             </div>

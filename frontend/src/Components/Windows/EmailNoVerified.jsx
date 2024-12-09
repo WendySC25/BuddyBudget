e59f8826ba9046cc869be9fcff8d50c9aa67f519
seldon1128 <seldon1128@gmail.com>
2024-12-08 18:19:47 -0600

    Cambios al front de verificacion de email

--- frontend/src/Components/Windows/EmailNoVerified.jsx
+++ frontend/src/Components/Windows/EmailNoVerified.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './EmailVerified.css';
-import logo__light from '../Assets/BuddyBudget_black.png';
+import './../LoginRegister/LoginRegister.css'; // Estilamos con el mismo CSS que Login/Register
+import { FaTimesCircle } from 'react-icons/fa';
 
-const EmailNoVerified = () => {
+const EmailVerified = () => {
   return (
-    <div className="email-verified">
-      <nav className="navbar">
-        <img src={logo__light} alt="Logo" className="logo" />
-      </nav>
-      <div className="content">
-        <h2>Email Verified Unsuccessfully</h2>
-        <p>Your account has not been verified.</p>
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
+    <div className="wrapper">
+      <div className="form-box">
+        <div className="content">
+          <FaTimesCircle className="verification-icon" />
+          <h1>Email Verified Unsuccessfully</h1>
+          <p>Your account has not been verified</p>
+          <Link to="/login">
+            <button className="login-button">Go to Login</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
-export default EmailNoVerified;
+export default EmailVerified;
--- frontend/src/Components/Windows/EmailVerified.jsx
+++ frontend/src/Components/Windows/EmailVerified.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './EmailVerified.css';
-import logo__light from '../Assets/BuddyBudget_black.png';
+import './../LoginRegister/LoginRegister.css'; // Estilamos con el mismo CSS que Login/Register
+import { FaCheckCircle } from 'react-icons/fa';
 
 const EmailVerified = () => {
   return (
-    <div className="email-verified">
-      <nav className="navbar">
-        <img src={logo__light} alt="Logo" className="logo" />
-      </nav>
-      <div className="content">
-        <h2>Email Verified Successfully</h2>
-        <p>Your account has been verified. You can now log in.</p>
-        <Link to="/login">
-          <button className="login-button">Login</button>
-        </Link>
+    <div className="wrapper">
+      <div className="form-box">
+        <div className="content">
+          <FaCheckCircle className="verification-icon" />
+          <h1>Email Verified Successfully</h1>
+          <p>Your account has been verified. You can now log in.</p>
+          <Link to="/login">
+            <button className="login-button">Go to Login</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default EmailVerified;
+
